07e010ca2d357cb6b382b9ddef33fd8304267088
fuad <m.fuadyerdelen@gmail.com>
2022-06-20 21:05:10 +0200

    Updates

---
+++ precache-manifest.8859557b97265293a003b088ef48b688.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ef3db7107060a2ba3f4",
+    "url": "/static/css/main.64ed8354.chunk.css"
+  },
+  {
+    "revision": "2ef3db7107060a2ba3f4",
+    "url": "/static/js/main.80dd0313.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dfe4299fcafbc4cfd349",
+    "url": "/static/js/2.a270c9da.chunk.js"
+  },
+  {
+    "revision": "2d6070c38009759a0fd64eb00401ac29",
+    "url": "/static/media/profil.2d6070c3.jpg"
+  },
+  {
+    "revision": "905165b55404bbf5e933676a67eb0a0f",
+    "url": "/static/media/twitter.905165b5.png"
+  },
+  {
+    "revision": "21f9458b4d0ab76b332083666083b6ae",
+    "url": "/static/media/reddit.21f9458b.png"
+  },
+  {
+    "revision": "fdba2dfcc122a0c4039a02a083992879",
+    "url": "/index.html"
+  }
+];
