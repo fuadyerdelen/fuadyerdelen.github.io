86d7892ee8e967da8e79de01b56a09f0ecfb9976
fuad <m.fuadyerdelen@gmail.com>
2022-06-20 21:01:00 +0200

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.a6fd7acaa1ec12501f491a00a3dfdf43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dba5c895c1b4f6b41ed4",
+    "url": "/static/css/main.b5db93bb.chunk.css"
+  },
+  {
+    "revision": "dba5c895c1b4f6b41ed4",
+    "url": "/static/js/main.422853cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dfe4299fcafbc4cfd349",
+    "url": "/static/js/2.a270c9da.chunk.js"
+  },
+  {
+    "revision": "2d6070c38009759a0fd64eb00401ac29",
+    "url": "/static/media/profil.2d6070c3.jpg"
+  },
+  {
+    "revision": "905165b55404bbf5e933676a67eb0a0f",
+    "url": "/static/media/twitter.905165b5.png"
+  },
+  {
+    "revision": "21f9458b4d0ab76b332083666083b6ae",
+    "url": "/static/media/reddit.21f9458b.png"
+  },
+  {
+    "revision": "0b5a03b1baa8ac7e04c0d27ff4890ab7",
+    "url": "/index.html"
+  }
+];
